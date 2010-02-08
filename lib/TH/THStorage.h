470322b865ae7badf09cb27714cc0f197f0e34b9
Arsène Lupin <collober@zorglub.local>
2010-02-07 22:23:46 -0500

    revamped torch5

---
+++ lib/TH/THStorage.h
@@ -0,0 +1,28 @@
+/* ils appellent tous la vtable... THStorage est un module apres tout.
+   donc newMappedFile est dans le module. voila.
+   et les autres servent pour le polymorphisme.
+   donc en cas de sous classe (genre MappedStorage! on peut toujours utiliser ca)
+   c'est juste que le module sous-classe sera d'un nom different. logique. */
+
+#include "THGeneral.h"
+
+typedef struct THStorage THStorage;
+
+extern const struct THMStorage
+{
+  THStorage* (*alloc)();
+
+  THStorage* (*allocWithSize)(long);
+  THStorage* (*allocMappedFile)(const char*, int);
+  
+  real* (*data)(THStorage*);
+  long (*size)(THStorage*);
+
+  void (*retain)(THStorage*);
+  void (*copy)(THStorage*, THStorage*);
+  void (*resize)(THStorage*, long);
+  
+  void (*free)(THStorage*);
+  
+} THMStorage;
+
